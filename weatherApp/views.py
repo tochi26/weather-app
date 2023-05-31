941b4aed9a2bbc2722aed4fb52ddfb165238d372
Tochukwu Nzewi <tochukwunzewi@Tochukwus-MacBook-Air.local>
2023-05-31 08:42:28 +0100

    made some url changes and added in error handling for when the country or state is not found

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import urllib.parse
 import urllib.request
 import json
 from dotenv import load_dotenv
@@ -6,31 +7,34 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-
-
-
 API_KEY = os.getenv('API_KEY')
-# Create your views here.
+
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}').read()
-        list_of_data = json.loads(source)
-        
-        data = {
-            "country_code" : str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ', '
-            + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + ' °C',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-            "main": str(list_of_data['weather'][0]['main']),
-            "description":  str(list_of_data['weather'][0]['description']),
-            "icon": list_of_data['weather'][0]['icon'],
-        }
+        encoded_city = urllib.parse.quote(city)
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={encoded_city}&units=metric&appid={API_KEY}'
 
-        print(data)
+        try:
+            source = urllib.request.urlopen(url).read()
+            list_of_data = json.loads(source)
+            
+            data = {
+                "country_code" : str(list_of_data['sys']['country']),
+                "coordinate": str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coord']['lat']),
+                "temp": str(list_of_data['main']['temp']) + ' °C',
+                "pressure": str(list_of_data['main']['pressure']),
+                "humidity": str(list_of_data['main']['humidity']),
+                "main": str(list_of_data['weather'][0]['main']),
+                "description":  str(list_of_data['weather'][0]['description']),
+                "icon": list_of_data['weather'][0]['icon'],
+            }
+
+            print(data)
+        except Exception as e:
+            print(f"Error occurred: {str(e)}")
+            data = {}  # Set an empty data dictionary in case of an error
     else:
         data = {}
 
-    return render(request, "main/index.html", data)    
+    return render(request, "main/index.html", data)
